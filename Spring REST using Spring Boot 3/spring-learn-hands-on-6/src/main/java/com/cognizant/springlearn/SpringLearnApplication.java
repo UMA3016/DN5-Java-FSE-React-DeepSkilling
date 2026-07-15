2579cd72c87359b66235bb42996958a88b5a4ebb
Uma Sri <umasriguttikonda@gmail.com>
2026-07-15 11:59:04 +0530

    Spring REST using Spring Boot 3

---
+++ Spring REST using Spring Boot 3/spring-learn-hands-on-6/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
@@ -0,0 +1,62 @@
+package com.cognizant.springlearn;
+
+import java.util.ArrayList;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SpringBootApplication
+public class SpringLearnApplication {
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(SpringLearnApplication.class);
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringLearnApplication.class, args);
+
+        // Uncomment the method required for the hands-on
+
+        // displayCountry();      // Hands-on 5
+        displayCountries();       // Hands-on 6
+    }
+
+    public static void displayCountry() {
+
+        LOGGER.info("START");
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+        Country country =
+                context.getBean("country", Country.class);
+
+        Country anotherCountry =
+                context.getBean("country", Country.class);
+
+        LOGGER.debug("Country 1 : {}", country);
+        LOGGER.debug("Country 2 : {}", anotherCountry);
+
+        LOGGER.info("END");
+    }
+
+    @SuppressWarnings("unchecked")
+    public static void displayCountries() {
+
+        LOGGER.info("START");
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+        ArrayList<Country> countryList =
+                (ArrayList<Country>) context.getBean("countryList");
+
+        LOGGER.debug("Countries : {}", countryList);
+
+        LOGGER.info("END");
+    }
+}
