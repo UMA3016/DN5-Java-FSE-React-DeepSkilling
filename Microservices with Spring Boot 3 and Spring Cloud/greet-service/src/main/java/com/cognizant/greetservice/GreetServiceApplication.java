2234585c60081e6b462c3b01000ea31670cfc674
Uma Sri <umasriguttikonda@gmail.com>
2026-07-19 14:17:52 +0530

    Added microservices exercises

---
+++ Microservices with Spring Boot 3 and Spring Cloud/greet-service/src/main/java/com/cognizant/greetservice/GreetServiceApplication.java
@@ -0,0 +1,11 @@
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GreetServiceApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(GreetServiceApplication.class,args);
+
+    }
+
+}
